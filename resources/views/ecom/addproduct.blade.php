2ed9257cc3df035cd86d671f74394816560451d6
Megazarth <ysutanto22@Gmail.com>
2018-05-31 12:49:18 +0700

    Add Picture Chooser
    Branch backup untuk berjaga2 jika ada apa2 dengan project. Kalau project yang dipush ke backup tidak ada masalah baru di push ke master, OK Bob?
    
    Update ini menambahkan Picture Chooser.

--- resources/views/ecom/addproduct.blade.php
+++ resources/views/ecom/addproduct.blade.php
@@ -76,6 +76,19 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="gambar" class="col-md-4 col-form-label text-md-right">{{ __('Product Picture') }}</label>
+
+                            <div class="col-md-6">
+                                {!! Form::file('gambar') !!}
+                                @if ($errors->has('gambar'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('gambar') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
